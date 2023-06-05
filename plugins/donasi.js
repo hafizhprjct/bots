837fd6754f954693fd195f19d5ba00496bf4592b
hafizhprjct <113893468+hafizhprjct@users.noreply.github.com>
2023-06-05 18:05:59 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,18 +1,18 @@
 let fs = require('fs')
 let handler = async (m, { conn, args, command }) => {
-let qris = 'https://telegra.ph/file/19f968d68c89c645f5191.jpg'
+let qris = 'https://telegra.ph/file/6303a7615d79c47f0035b.jpg'
 let dann =
 `
 ┏━━━ꕥ〔 *${global.wm2}* 〕ꕥ━⬣ 
-┃Dana: 083137550315
-┃Pulsa: 083137550315 ( XL )
-┃Pulsa: 0895342959060 ( 3 )
+┃Dana: 085651307830
+┃Pulsa: 085651307830 ( Im3 )
+┃Pulsa: 081354060278 ( Telkomsel )
 ┃Gopay: 083137550315
 ┗━━━ꕥ
 ┏━━━ꕥ〔 *Website* 〕ꕥ━⬣ 
-┃Saweria: https://saweria.co/DannXD
-┃Trakteer: https://trakteer.id/DannXD
-┃Socialbuzz: https://socialbuzz.com/DannXD
+┃Saweria: https://saweria.co/HafizhFlash
+┃Trakteer: https://trakteer.id/HafizhFlash22
+┃Socialbuzz: https://socialbuzz.com/hafizh_flash
 ┃Thanks for Donationing!
 ┗━━━ꕥ
 `
